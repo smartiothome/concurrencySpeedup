aef2d36a90be6866d05d9f92c8a904fc60aad16a
Smart IOT Home <smartiothome@icloud.com>
2016-03-07 21:48:48 -0800

    Adding PhotoRecord class files that lets you efficiently refer to Photos

---
+++ PhotosWorthSharing/PhotoRecord.m
@@ -0,0 +1,14 @@
+//
+//  PhotoRecord.m
+//  PhotosWorthSharing
+//
+//  Created by Smart Home on 3/6/16.
+//  Copyright © 2016 Smart Home. All rights reserved.
+//
+
+#import "PhotoRecord.h"
+
+@implementation PhotoRecord
+
+
+@end
